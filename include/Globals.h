36afb0ca1c03ae372abf7820e5fa75dd1a953106
Ethan Cavadia <cavadiaethan@gmail.com>
2018-11-27 16:37:56 +0100

    commit

--- include/Globals.h
+++ include/Globals.h
@@ -2,13 +2,8 @@
 #define GLOBALS_H
 #include <string>
 
-<<<<<<< HEAD
 	const auto WINDOW_WIDTH = 1024u;
 	const auto WINDOW_HEIGTH = 720u;
-=======
-	const float WINDOW_WIDTH = 1024.0f;
-	const float WINDOW_HEIGTH = 720.0f;
->>>>>>> master
 	const float PLAYER_SPAWN = 100.0f;
 	const float GRAVITY_ = 9.81f;
 	const float PLAYER_WIDTH = 64.0f;
